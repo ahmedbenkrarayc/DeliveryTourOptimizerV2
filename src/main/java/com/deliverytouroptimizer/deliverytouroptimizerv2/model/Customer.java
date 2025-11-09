8417c66ecbaa1121df2b6116cc1989ba02785150
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-11-10 00:54:19 +0100

    DTO-92 Define Customer Fields

---
+++ src/main/java/com/deliverytouroptimizer/deliverytouroptimizerv2/model/Customer.java
@@ -0,0 +1,37 @@
+package com.deliverytouroptimizer.deliverytouroptimizerv2.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "First name cannot be blank")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    private String fname;
+
+    @NotBlank(message = "Last name cannot be blank")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    private String lname;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Email must be a valid email address")
+    private String email;
+
+    @Pattern(regexp = "^\\+?[0-9]{7,15}$", message = "Phone number must be valid")
+    private String phone;
+}
