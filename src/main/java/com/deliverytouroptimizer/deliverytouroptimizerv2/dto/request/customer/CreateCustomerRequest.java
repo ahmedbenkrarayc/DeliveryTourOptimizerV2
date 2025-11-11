9121bf9a9a4bf85e902b33861b214f629711d4e1
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-11-12 00:26:07 +0100

    DTO-130 Create Customer requests for update and create

---
+++ src/main/java/com/deliverytouroptimizer/deliverytouroptimizerv2/dto/request/customer/CreateCustomerRequest.java
@@ -0,0 +1,25 @@
+package com.deliverytouroptimizer.deliverytouroptimizerv2.dto.request.customer;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record CreateCustomerRequest(
+        @NotBlank(message = "First name is required")
+        @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+        String fname,
+        @NotBlank(message = "Last name is required")
+        @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+        String lname,
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+        @NotBlank(message = "Phone number is required")
+        @Pattern(
+                regexp = "^(\\+?[0-9]{7,15})$",
+                message = "Phone number must be valid and contain only digits, optionally starting with +"
+        )
+        String phone
+) {
+}
