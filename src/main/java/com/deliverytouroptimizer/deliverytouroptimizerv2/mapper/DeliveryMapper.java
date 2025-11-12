c6505ee87f2b6a7915de7b5367e3f3153c220ae4
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-11-12 10:05:36 +0100

    added customerId to DeliveryMapper

--- src/main/java/com/deliverytouroptimizer/deliverytouroptimizerv2/mapper/DeliveryMapper.java
+++ src/main/java/com/deliverytouroptimizer/deliverytouroptimizerv2/mapper/DeliveryMapper.java
@@ -3,6 +3,7 @@ package com.deliverytouroptimizer.deliverytouroptimizerv2.mapper;
 import com.deliverytouroptimizer.deliverytouroptimizerv2.dto.request.delivery.CreateDeliveryRequest;
 import com.deliverytouroptimizer.deliverytouroptimizerv2.dto.request.delivery.UpdateDeliveryRequest;
 import com.deliverytouroptimizer.deliverytouroptimizerv2.dto.response.delivery.DeliveryResponse;
+import com.deliverytouroptimizer.deliverytouroptimizerv2.model.Customer;
 import com.deliverytouroptimizer.deliverytouroptimizerv2.model.Delivery;
 import com.deliverytouroptimizer.deliverytouroptimizerv2.model.Tour;
 import org.mapstruct.*;
@@ -10,10 +11,12 @@ import org.mapstruct.*;
 @Mapper(componentModel = "spring", uses = { TourMapper.class })
 public interface DeliveryMapper {
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "tour", expression = "java(toTour(dto.tourId()))")
-    Delivery toEntity(CreateDeliveryRequest dto);
+    @Mapping(target = "tour", expression = "java(toTour(request.tourId()))")
+    @Mapping(target = "customer", expression = "java(toCustomer(request.customerId()))")
+    Delivery toEntity(CreateDeliveryRequest request);
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(target = "tour", expression = "java(toTour(request.tourId()))")
+    @Mapping(target = "customer", expression = "java(toCustomer(request.customerId()))")
     void updateEntityFromDto(UpdateDeliveryRequest request, @MappingTarget Delivery delivery);
     DeliveryResponse toResponse(Delivery delivery);
 
@@ -22,5 +25,12 @@ public interface DeliveryMapper {
         Tour tour = new Tour();
         tour.setId(tourId);
         return tour;
+    }
+
+    default Customer toCustomer(Long customerId) {
+        if (customerId == null) return null;
+        Customer customer = new Customer();
+        customer.setId(customerId);
+        return customer;
     }
 }
