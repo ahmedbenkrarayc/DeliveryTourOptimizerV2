74eefac429af968a7355931bc42955b3fcff3033
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-11-12 01:17:39 +0100

    DTO-136 Create DeliveryHistory create & update requests

---
+++ src/main/java/com/deliverytouroptimizer/deliverytouroptimizerv2/dto/request/deliveryhistory/CreateDeliveryHistoryRequest.java
@@ -0,0 +1,19 @@
+package com.deliverytouroptimizer.deliverytouroptimizerv2.dto.request.deliveryhistory;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record CreateDeliveryHistoryRequest(
+        @NotNull(message = "Delivery date is required")
+        @FutureOrPresent(message = "Delivery date cannot be in the past")
+        LocalDate deliveryDate,
+        @NotNull(message = "Planned time is required")
+        LocalTime plannedTime,
+        LocalTime actualTime,
+        @NotNull(message = "Delivery ID is required")
+        Long deliveryId
+) {
+}
